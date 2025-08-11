ff906773d0714b48f9efec0aeda5dc37963f55f6
Clickonuser <clickonuser@gmail.com>
2025-08-12 01:02:24 +0300

    add portrait orientation for activity_coin_price_list.xml

--- app/src/main/java/com/example/coins/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/coins/presentation/CoinPriceListActivity.kt
@@ -3,6 +3,7 @@ package com.example.coins.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.coins.R
 import com.example.coins.databinding.ActivityCoinPriceListBinding
 import com.example.coins.domain.CoinInfo
 import com.example.coins.presentation.adapters.CoinInfoAdapter
@@ -23,8 +24,11 @@ class CoinPriceListActivity : AppCompatActivity() {
         binding.rvCoinPriceList.itemAnimator = null
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
-                val intent = CoinDetailActivity.newIntent(this@CoinPriceListActivity, coinPriceInfo.fromSymbol)
-                startActivity(intent)
+                if (isOnePaneMode()) {
+                    launchDetailActivity(coinPriceInfo.fromSymbol)
+                } else {
+                    launchDetailFragment(coinPriceInfo.fromSymbol)
+                }
             }
         }
 
@@ -33,5 +37,24 @@ class CoinPriceListActivity : AppCompatActivity() {
         viewModel.coinInfoList.observe(this) {
             adapter.submitList(it)
         }
+    }
+
+    private fun isOnePaneMode() = binding.fragmentContainer == null
+
+    private fun launchDetailActivity(fromSymbol: String) {
+        val intent = CoinDetailActivity.newIntent(
+            this@CoinPriceListActivity,
+            fromSymbol
+        )
+        startActivity(intent)
+    }
+
+    private fun launchDetailFragment(fromSymbol: String) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+            .addToBackStack(null)
+            .commit()
     }
 }
